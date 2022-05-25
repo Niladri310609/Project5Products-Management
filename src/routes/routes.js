0cd548157dc2fb9c4215dcea9debc9c07728a364
Niladri Sekhar Mukherjee <niladrisekharmukherjee3199@gmail.com>
2022-05-25 10:31:47 +0530

    changes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const{createUser} =require('../controllers/mz')
 const {updateUser}=require('../controllers/Nil')
 const {loginUser}=require('../controllers/Nil')
-
+const {userAuth}=require('../middleware/auth')
 
 
 
